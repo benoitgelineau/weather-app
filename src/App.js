47d5b13ba185dc496b189d5c37c420b602fc28d6
Benoît Gelineau <bgelineau.dev@gmail.com>
2019-02-14 14:56:52 +0100

    Fix syntax

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import style from './App.css';
 import WeatherList from './Components/WeatherList/WeatherList';
-import API_KEY from './secrets';
+import style from './App.css';
 
 class App extends Component {
   constructor(props) {
@@ -10,58 +9,59 @@ class App extends Component {
     this.state = {
       coord: {
         lat: '',
-        lon: ''
+        lon: '',
       },
       data: {},
       prevView: 0,
       expanded: false,
       error: false,
-      errMessage: ''
-    }
+      errMessage: '',
+    };
 
+    this.getCoordinates = this.getCoordinates.bind(this);
+    this.fetchData = this.fetchData.bind(this);
     this.handleClick = this.handleClick.bind(this);
-  }
-
-  getCoordinates() {
-    if (navigator.geolocation) {
-
-      navigator.geolocation.getCurrentPosition(pos => {
-        this.setState({
-          coord: {
-            lat: pos.coords.latitude.toFixed(4),
-            lon: pos.coords.longitude.toFixed(4)
-          }
-        });
-        this.fetchData();
-      }, error => {
-        console.warn(error);
-        this.setState({
-          error: true,
-          errMessage: 'Could not access geolocation'
-        })
-      })
-    }
-  }
-
-  fetchData() {
-    const { lat, lon } = this.state.coord;
-    const url = `http://api.openweathermap.org/data/2.5/forecast?APPID=${API_KEY}&lat=${lat}&lon=${lon}&units=metric`;
-
-    fetch(url)
-      .then(res => res.json())
-      .then(data => this.setState({ data: data }))
-      .catch(error => console.warn(error))
-  }
-  
-  handleClick(el) {
-    this.setState(prevState => ({
-      prevView: el,
-      expanded: el !== prevState.prevView ? (prevState.expanded ? prevState.expanded : !prevState.expanded) : !prevState.expanded
-    }))
   }
 
   componentDidMount() {
     this.getCoordinates();
+  }
+
+  getCoordinates() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((pos) => {
+        this.setState({
+          coord: {
+            lat: pos.coords.latitude.toFixed(4),
+            lon: pos.coords.longitude.toFixed(4),
+          },
+        }, this.fetchData());
+      }, (error) => {
+        console.warn(error);
+        this.setState({
+          error: true,
+          errMessage: 'Could not access geolocation',
+        });
+      }, { timeout: 10000 });
+    }
+  }
+
+  fetchData() {
+    const { coord: { lat, lon } } = this.state;
+    const url = `http://api.openweathermap.org/data/2.5/forecast?APPID=${process.env.API_KEY}&lat=${lat}&lon=${lon}&units=metric`;
+
+    fetch(url)
+      .then(res => res.json())
+      .then(data => this.setState({ data }))
+      .catch(error => console.warn(error));
+  }
+
+  handleClick(el) {
+    this.setState(({ prevView, expanded }) => ({
+      prevView: el,
+      // expanded: el !== prevState.prevView ? (prevState.expanded ? prevState.expanded : !prevState.expanded) : !prevState.expanded,
+      expanded: el !== prevView ? (expanded ? expanded : !expanded) : !expanded,
+    }));
   }
 
   render() {
@@ -71,13 +71,14 @@ class App extends Component {
     if (error) {
       display = <h1>{errMessage}</h1>;
     } else {
-      display = 
-        <WeatherList 
-          data={data} 
+      display = (
+        <WeatherList
+          data={data}
           prevView={prevView}
           expanded={expanded}
           onClick={this.handleClick}
         />
+      );
     }
 
     return (
